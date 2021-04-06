026e295f253d76497520898b4412bee3adbce19b
Richard <thibault.richard2000@gmail.com>
2021-04-06 19:44:44 +0200

    :sparkles: add video

---
+++ src/components/03_organisms/Media/Media.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container--media">
+    <iframe
+      class="video"
+      src="https://www.youtube.com/embed/E3tlWFyzG5o?autoplay=1&mute=1&controls=0&loop=1&showinfo=0&playlist=E3tlWFyzG5o&modestbranding=1&iv_load_policy=3"
+      frameborder="0"
+    ></iframe>
+    <div class="media__content">
+      <CpText type="hero-title">LV Watches</CpText>
+      <CpText type="hero">Travel in time</CpText>
+      <div class="downloads">
+        <ButtonStore brand="apple"/>
+        <ButtonStore brand="android"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CpText from "@/components/01_atoms/CpText/CpText.vue";
+import ButtonStore from "@/components/02_molecules/ButtonStore/ButtonStore.vue";
+
+export default {
+  name: "Media",
+  components: {
+    CpText,
+    ButtonStore
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./Media.scss";
+</style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -4,6 +4,7 @@
     <Hero/>
     <Articles />
     <Carousel />
+    <Media />
     <Footer />
   </div>
 </template>
@@ -14,6 +15,7 @@ import Header from "@/components/03_organisms/Header/Header.vue";
 import Hero from "@/components/03_organisms/Hero/Hero.vue";
 import Articles from "@/components/03_organisms/Articles/Articles.vue";
 import Carousel from "@/components/03_organisms/Carousel/Carousel.vue";
+import Media from "@/components/03_organisms/Media/Media.vue";
 import Footer from "@/components/03_organisms/Footer/Footer.vue";
 
 export default {
@@ -23,6 +25,7 @@ export default {
     Hero,
     Articles,
     Carousel,
+    Media,
     Footer
   },
   data() {
